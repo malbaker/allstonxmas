417c4179911764d01ace99d4fcb687d21b7b4b02
Malik Baker <78194321+malbaker@users.noreply.github.com>
2023-01-30 20:22:41 -0500

    removed glitchy head link

--- app/head.tsx
+++ app/head.tsx
@@ -5,10 +5,7 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="DIBS: An AllstonXmas Product" />
       <link rel="icon" href="/favicon.ico" />
-      <link
-          href="https://fonts.googleapis.com/css2?family=Caveat:wght@700&display=swap"
-          rel="stylesheet"
-        />
+      
     </>
   )
 }
